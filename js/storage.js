4555c9553cff04128d3e8100604a1df4172f767a
MarioBengo <Mariobengolea6@gmail.com>
2023-10-24 22:25:28 -0300

    first commit

--- js/storage.js
+++ js/storage.js
@@ -1,37 +1,37 @@
 let consolasPrimerInicio = [
 	{
 		id: "5f49fab9-3135-4676-a160-5c3fdbb1ae92",
-		descripcion: "Consola de hogar con gráficos en alta definición.",
-		titulo: "PlayStation 5",
-		precio: 499.99,
-		cuotas: "12 cuotas $250",
+		descripcion: "La batería electrónica Alesis Strike Pro Special Edition B-Stock es un kit de batería electrónica con características especiales que se encuentra en condición B-Stock.",
+		titulo: "BATERIA ELECTRONICA ALESIS STRIKE PRO SPECIAL EDITION B-STOCK",
+		precio: 100000.00,
+		cuotas: "12 cuotas $10.000",
 		imagen:
-			"https://stylewatch.vtexassets.com/arquivos/ids/233580-800-800?v=638188815205330000&width=800&height=800&aspect=true",
-		categoria: "consolas",
+			"https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/screenshot_14-451f71aa930b0320e716693795749392-640-0.png",
+		categoria: "Baterias",
 		fechaDeCreacion: "2020-11-12",
 	},
 	{
 		id: "2e897bad-d4e4-413d-a515-ed95df9ad917",
 		fechaDeCreacion: "2017-03-03",
-		descripcion: "Consola portátil con pantalla táctil.",
-		titulo: "Nintendo Switch",
-		precio: 299.99,
-		cuotas: "12 cuotas $250",
+		descripcion: "La guitarra eléctrica Jet Guitars JS450 QTBR es una Strato-caster con pastillas HSS que ofrece un sonido versátil y un diseño clásico.",
+		titulo: "GUITARRA ELECTRICA JET GUITARS JS450",
+		precio: 150000.00,
+		cuotas: "12 cuotas $25.000",
 		imagen:
-			"https://http2.mlstatic.com/D_NQ_NP_625423-MLA47920375564_102021-O.webp",
-		categoria: "accesorios",
+			"https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/img_20230803_1454385201-6d9cb77f3d7e4dc1a816910868652770-640-0.jpg",
+		categoria: "Guitarras",
 	},
 	{
 		id: "fc3025ec-f314-4b63-9765-1e8df3ee358a",
 		descripcion:
-			"Consola de hogar con alta potencia y compatibilidad hacia atrás.",
-		titulo: "Xbox Series X",
+			"perfecto para músicos que buscan versatilidad en un paquete compacto.",
+		titulo: "UKULELE STAGG US80 SE SOPRANO CON EQ",
 		fechaDeCreacion: "2020-11-10",
-		precio: 499.99,
-		cuotas: "12 cuotas $250",
+		precio: 70000.00,
+		cuotas: "4 cuotas $20.000",
 		imagen:
-			"https://assets.xboxservices.com/assets/fb/d2/fbd2cb56-5c25-414d-9f46-e6a164cdf5be.png?n=XBX_A-BuyBoxBGImage01-D.png",
-		categoria: "juegos",
+			"https://d3ugyf2ht6aenh.cloudfront.net/stores/969/083/products/stagg-ut70-021-a8cb624943afbf54d216014113677292-640-0.jpg",
+		categoria: "Otros",
 	},
 ];
 
@@ -44,7 +44,7 @@ if (JSON.parse(localStorage.getItem("productos")) === null) {
 
 const userInicio = [
 	{
-		fullname: "Daniel Lee",
+		fullname: "Daniel",
 		email: "admin@admin.com",
 		id: "6",
 		password: "admin",
